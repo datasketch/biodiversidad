1887b1cf8084bdcc28eed0a8b4b8bf0b66e43cfa
David Daza <03dazal@gmail.com>
2021-12-17 10:53:59 -0500

    Configure tw props. Add partials

--- tailwind.config.js
+++ tailwind.config.js
@@ -12,6 +12,14 @@ module.exports = {
         'open-sans': ['"open sans"', 'sans-serif'],
         oswald: ['oswald', 'sans-serif'],
         'open-sans-condensed': ['"open sans condensed"', 'sans-serif']
+      },
+      colors: {
+        'command-blue': '#1C75AD',
+        xiketic: '#0E1223',
+        'eerie-black': '#1A1A1A',
+        cultured: '#FBF9F8',
+        bittersweet: '#ED6B60',
+        forest: '#217866'
       }
     },
   },

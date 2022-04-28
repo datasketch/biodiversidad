d7f8012b510854e2969b40b806c409bd3dd1d73f
Yosman Andres Higuita Gonzalez <yosman.higuita028@gmail.com>
2022-04-28 15:31:40 -0500

    add sliders, add menu mobile

--- assets/js/default.js
+++ assets/js/default.js
@@ -6,3 +6,29 @@
 //   menuName.classList.toggle('menu__name--active')
 //   menuPanel.classList.toggle('menu__panel--active')
 // })
+
+'use strict'
+
+// ELEMENTS
+const navMobile = document.querySelector('.nav')
+const buttonMenu = document.querySelector('.menu-button')
+const iconMenu = document.querySelector('.menu-icon')
+
+// FUNCTIONS
+const menuToggle = () => {
+  // SHOW MENU BAR
+  navMobile.classList.toggle('nav--active')
+  // SHOW ICON TRANSITION
+  iconMenu.classList.toggle('menu-icon--active')
+}
+
+// EVENTS HANDLERS
+buttonMenu.addEventListener('click', menuToggle)
+
+window.addEventListener('scroll', (e) => {
+  if (e.isTrusted && navMobile.classList.contains('nav--active')) menuToggle()
+})
+
+window.addEventListener('keydown', (e) => {
+  if (e.key === 'Escape' && navMobile.classList.contains('nav--active')) menuToggle()
+})

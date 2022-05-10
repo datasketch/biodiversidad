dd315c8d01f99567af9e640cc78df66e108574d5
Yosman Andres Higuita Gonzalez <yosman.higuita028@gmail.com>
2022-05-10 16:09:10 -0500

    add react init

---
+++ assets/js/ventana.js
@@ -0,0 +1,10 @@
+/* eslint-disable no-unused-vars */
+import React, { useState } from 'react'
+import ReactDOM from 'react-dom'
+
+ReactDOM.render(
+  <React.StrictMode>
+    <div>Prueba</div>
+  </React.StrictMode>,
+  document.getElementById('app')
+)

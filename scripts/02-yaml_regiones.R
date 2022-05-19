c15276cd7badc364057f23539e7318c3437ed4f5
David Daza <03dazal@gmail.com>
2022-05-19 16:38:52 -0500

    Update data script

--- scripts/02-yaml_regiones.R
+++ scripts/02-yaml_regiones.R
@@ -16,36 +16,37 @@ reg <- reg[[1]]
 
 frontmatter <- list()
 frontmatter$title <- reg$label
-frontmatter$slug <- reg$slug
-frontmatter$fecha_corte <- as.character(as.Date(reg$fecha_corte, origin = "1970-01-01"))
-frontmatter$layout <- "single"
+# frontmatter$slug <- reg$slug
+frontmatter$type <- "departamentos"
 frontmatter$featured <- TRUE
+frontmatter$layout <- "single"
 frontmatter$image <-"/images/regiones/departamentos/narino.jpg"
 frontmatter$bgImage <- "/images/regiones/departamentos/banner-narino.png"
-frontmatter$include_marino <- deptos_divipola |> 
+frontmatter$include_marino <- deptos_divipola |>
   filter(slug == sel_region) |> pull(marino)
-frontmatter$especies <- reg$especies
+frontmatter$especies_registradas <- reg$especies
 frontmatter$especies_continentales <- reg$especies_continentales
 frontmatter$especies_marinas <- reg$especies_marinas
 frontmatter$especies <- reg$observaciones
 frontmatter$observaciones_continentales <- reg$observaciones_continentales
 frontmatter$observaciones_marinas <- reg$observaciones_marinas
+frontmatter$fecha_corte <- as.character(as.Date(reg$fecha_corte, origin = "1970-01-01"))
 
 # Temática
-tematica_json_path <- glue::glue("data/{sel_region}_tematica.json")
-jsonlite::write_json(reg, 
-                     paste0("../static/",tematica_json_path),
+tematica_json_path <- glue::glue("{sel_region}_tematica.json")
+jsonlite::write_json(reg,
+                     paste0("../data/",tematica_json_path),
                      auto_unbox = TRUE, pretty = TRUE)
 frontmatter$json_tematica <- tematica_json_path
 
 # Grupos biologicos
 grupo_biologico <- read_delim("db-cifras-sib/grupos_biologicos.tsv")
 
-gr_bio_sel_region <- grupo_biologico |> 
-  filter(slug_region == sel_region) 
-gr_bio_json_path <- glue::glue("data/{sel_region}_gr_bio.json")
-jsonlite::write_json(gr_bio_sel_region, 
-                     paste0("../static/",gr_bio_json_path),
+gr_bio_sel_region <- grupo_biologico |>
+  filter(slug_region == sel_region)
+gr_bio_json_path <- glue::glue("{sel_region}_gr_bio.json")
+jsonlite::write_json(gr_bio_sel_region,
+                     paste0("../data/",gr_bio_json_path),
                      auto_unbox = TRUE, pretty = TRUE)
 frontmatter$json_grupos_biologicos <- gr_bio_json_path
 
@@ -54,47 +55,47 @@ arbol_regiones <- read_delim("db-cifras-sib/arbol_regiones.tsv")
 arbol_regiones <- arbol_regiones |> filter(parent == sel_region)
 tree <- FromDataFrameNetwork(arbol_regiones)
 tree
-l <- ToListExplicit(tree, unname = TRUE, nameName = "slug", 
+l <- ToListExplicit(tree, unname = TRUE, nameName = "slug",
                     childrenName = "children")
 l
-subreg_json_path <- glue::glue("data/{sel_region}_subreg.json")
-jsonlite::write_json(l, 
-                     paste0("../static/",subreg_json_path),
+subreg_json_path <- glue::glue("{sel_region}_subreg.json")
+jsonlite::write_json(l,
+                     paste0("../data/",subreg_json_path),
                      auto_unbox = TRUE, pretty = TRUE)
 frontmatter$json_subregiones <- subreg_json_path
 
 # Temáticas Especies
 especie_tematica <- read_delim("db-cifras-sib/especie_tematica.tsv")
-esp_tematica_sel_region <- especie_tematica |> 
-  filter(slug_region == sel_region) |> 
-  select(-slug_region) |> 
+esp_tematica_sel_region <- especie_tematica |>
+  filter(slug_region == sel_region) |>
+  select(-slug_region) |>
   group_by(slug_tematica)
-x <- esp_tematica_sel_region |> 
-  group_split() |> 
-  set_names(unlist(group_keys(esp_tematica_sel_region)))  
-tematica_esp_json_path <- glue::glue("data/{sel_region}_tematica_esp.json")
-jsonlite::write_json(x, 
-                     paste0("../static/",tematica_esp_json_path),
+x <- esp_tematica_sel_region |>
+  group_split() |>
+  set_names(unlist(group_keys(esp_tematica_sel_region)))
+tematica_esp_json_path <- glue::glue("{sel_region}_tematica_esp.json")
+jsonlite::write_json(x,
+                     paste0("../data/",tematica_esp_json_path),
                      auto_unbox = TRUE, pretty = TRUE)
 frontmatter$json_tematica_esp <- tematica_esp_json_path
 
 
 # Publicadores
-pubs_sel_region <- publicadores |> 
+pubs_sel_region <- publicadores |>
   filter(slug_region == sel_region)
-pubs_json_path <- glue::glue("data/{sel_region}_pubs.json")
-jsonlite::write_json(pubs_sel_region, 
-                     paste0("../static/",pubs_json_path),
+pubs_json_path <- glue::glue("{sel_region}_pubs.json")
+jsonlite::write_json(pubs_sel_region,
+                     paste0("../data/",pubs_json_path),
                      auto_unbox = TRUE, pretty = TRUE)
 frontmatter$json_publicadores <- pubs_json_path
 
 # Write YAML
-
 yaml <- paste("---",
+      "menu:\n\tmain:\n\t\tparent: departamentos",
       yaml::as.yaml(frontmatter),
       "---\n\n",
       sep = "\n")
-write_lines(yaml, glue::glue("../content/",sel_region,".md"))
+write_lines(yaml, glue::glue("../content/regiones/departamentos/",sel_region,"/_index.md"))
 
 
 
